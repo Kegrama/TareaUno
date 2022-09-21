f6299b05e79a1111d13a693c94665f5dab2491e6
Kegrama <kgmpiano1@hotmail.com>
2022-09-20 19:05:04 -0600

    Tarea 1

---
+++ Tarea1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progra2_Clase1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tipos de variables
+
+            int num1, num3, num4; //Variable entera
+            int num2 = 0;
+            long alrga = -1222222;
+            byte b = 255;
+            float num5 = 0; //Variable flotante para decimales
+            double num6 = 0; //Variable doble para decimales, mayor
+
+            string nombre = "Marcia"; //Variable cadena de caracteres
+            char opcion = 'S'; //Variable char solo un aracter
+
+            Boolean boleana = true;
+
+            num1 = num2 * 2;
+
+            //Tarea: investigar los siguientes tipos de datos:
+
+            dynamic dinamico = 10;
+            var variable = 52;
+        }
+    }
+}
